51614ae0f5e92560341a8c5086a463f9f579be57
jun <juha@ucsd.edu>
2023-10-29 22:17:40 -0700

    finished styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [imageList, setImageList] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
 
   const handleDirectoryChange = (e) => {
     const files = [...e.target.files];
@@ -13,20 +15,75 @@ function App() {
   };
 
   const handleImageClick = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % imageList.length);
+    if (isPlaying) {
+      togglePlay();
+    } else {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % imageList.length);
+    }
   };
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === 'Space') {
+        handleImageClick();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [imageList]);
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    } else {
+      const id = setInterval(handleImageClick, 500);
+      setIntervalId(id);
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [intervalId]);
 
   return (
     <div className="App">
-      <input type="file" directory="" webkitdirectory="" onChange={handleDirectoryChange} />
+      <div id="display" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <label className="customFileInput">
+          Choose Files
+          <input type="file" directory="" webkitdirectory="" onChange={handleDirectoryChange} style={{ display: 'none' }} />
+        </label>
+        <button className="play" onClick={togglePlay}>{isPlaying ? "Stop" : "Play"}</button>
+      </div>
 
       {imageList.length > 0 && (
-        <img 
-          src={URL.createObjectURL(imageList[currentIndex])} 
-          alt="Selected directory content" 
-          style={{width: '50%', margin: '20px auto', display: 'block'}} 
+        <img
+          src={URL.createObjectURL(imageList[currentIndex])}
+          alt="Selected directory content"
+          style={{ width: '50%', margin: '20px auto', display: 'block' }}
           onClick={handleImageClick}
         />
+      )}
+
+      {imageList.length > 0 && (
+        <div style={{
+          position: 'fixed',
+          bottom: '5%',
+          width: '100%',
+          textAlign: 'center',
+          color: 'black'
+        }}>
+          {currentIndex + 1} of {imageList.length}
+        </div>
       )}
     </div>
   );
