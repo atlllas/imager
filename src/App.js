216e33d5a82b1ef4b4aa953726f26422019008b0
jun <juha@ucsd.edu>
2023-10-29 21:31:46 -0700

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [imageList, setImageList] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleDirectoryChange = (e) => {
+    const files = [...e.target.files];
+    const imageFiles = files.filter(file => file.type.startsWith('image/'));
+    setImageList(imageFiles);
+    setCurrentIndex(0);
+  };
+
+  const handleImageClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % imageList.length);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" directory="" webkitdirectory="" onChange={handleDirectoryChange} />
+
+      {imageList.length > 0 && (
+        <img 
+          src={URL.createObjectURL(imageList[currentIndex])} 
+          alt="Selected directory content" 
+          style={{width: '50%', margin: '20px auto', display: 'block'}} 
+          onClick={handleImageClick}
+        />
+      )}
     </div>
   );
 }
